d6258bc7aee7da7d6f3652a369dba1a631e3d3b0
KotiMango <rons20000@gmail.com>
2021-05-10 12:02:36 +0300

    9. Initializing React Router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //Import pages
 import AboutUs from "../src/pages/AboutUs";
+import ContactUs from "../src/pages/ContactUs";
+import OurWork from "../src/pages/OurWork";
 //Import Components
 import Nav from "../src/components/Nav";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
